28fda2f22e3c45aaaa9cd8f1928236a643f263c8
alyson <alyson@desktop-ti-dev.localdomain>
2022-07-01 13:54:16 -0300

    feat: adicionando itens no componente Experiencias

---
+++ src/components/Experiencias/Empresas.tsx
@@ -0,0 +1,5 @@
+export function Empresas(name, key) {
+    return <div className="ml-3 w-32 h-12 text-zinc-500">
+        <p>{name}</p>
+    </div>;
+}
--- src/components/Experiencias/Experiencias.tsx
+++ src/components/Experiencias/Experiencias.tsx
@@ -1,7 +1,25 @@
+import { Empresas } from "./Empresas"
+
+const empresas=[
+{nome : "Digital House"},
+{nome: "Código Fonte TV"},
+{nome: "Zuplae"},
+{nome: "ContWeb"},
+{nome: "iuricode"},
+{nome: "Freelancer"}
+]
+
 export default function Experiencias(){
     return(
-        <div>
-            <h1>Experiências</h1>
-        </div>
+        // <div className="w-full h-2/3 bg-zinc-800">
+            <div className="px-48">
+                <h1 className="text-white text-2xl">EXPERIÊNCIA</h1>
+                <div className="mt-10 border-l border-zinc-300">
+                    {empresas.map((name, index) => <Empresas name={empresas.nome} key={index}/>)}
+                </div>
+            </div>
+        // </div>
     )
 }
+
+
